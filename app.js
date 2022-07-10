3522b174e440b0485a26432a75cef27193c6b4bf
Fendy Heryanto <fendy3002@gmail.com>
2022-07-10 18:02:31 +0800

    Change homepage

--- app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-import fs from 'fs';
-
-app.use(express.static(__dirname));
-
-app.listen(3000, function () {
-  console.log('String tools app listening on port 3000!')
-});
